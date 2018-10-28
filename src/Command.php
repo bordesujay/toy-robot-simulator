a6d3c118db73e0e3eabd773235676fcef3300769
Sujay Borde <bordesujay@gmail.com>
2018-10-28 22:37:25 +1100

    add Command class as enum type of class

---
+++ src/Command.php
@@ -0,0 +1,13 @@
+<?php
+
+/***
+ * Class Command - acts as enum
+ */
+class Command extends SplEnum
+{
+     const PLACE = 'PLACE';
+     const MOVE = 'MOVE';
+     const LEFT = 'LEFT';
+     const RIGHT = 'RIGHT';
+     const REPORT = 'REPORT';
+}
