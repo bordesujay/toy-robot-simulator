473a4dfbd2d0650cbc31c96f79533aa0b1e852e5
Sujay Borde <bordesujay@gmail.com>
2018-10-28 20:03:14 +1100

    add interface to add better abstraction level
    1. add surface interface

--- src/Surface.php
+++ src/Surface.php
@@ -1,6 +1,11 @@
 <?php
 
+require_once('Position.php');
+
+/***
+ * Interface Surface - to implement isValidPosition for surface of any shape and size
+ */
 interface Surface
 {
-
+    public function isValidPosition(Position $position);
 }
