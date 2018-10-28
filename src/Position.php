dd6c2c1467122d39ead3df5a72f6600addd4d759
Sujay Borde <bordesujay@gmail.com>
2018-10-28 20:03:35 +1100

    add positon class
    this class will hold the value of position

---
+++ src/Position.php
@@ -0,0 +1,23 @@
+<?php
+
+class Position
+{
+    private $x;
+    private $y;
+
+    /**
+     * @return mixed
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+}
